c0528314b895b93329fedd8b58c455537bcc8177
Joseph Moberly <jmobes15@gmail.com>
2022-06-02 23:54:50 -0700

    Separated createTable function into smaller functions for easier reading + debugging.

--- main.js
+++ main.js
@@ -7,21 +7,39 @@ window.addEventListener("load", (event) => {
 
   function createTable(num = 100) {
     const numOfTableCells = document.getElementById("cells").value || num;
-    if (!numOfTableCells || numOfTableCells > 300 || numOfTableCells < 1)
-      return;
-
-    const reset = resetTable();
-    if (!reset) {
+    if (!numOfTableCells || numOfTableCells > 300 || numOfTableCells < 1) {
       return;
     }
 
     const tableContainer = document.querySelector(".table");
+
+    const reset = resetTable(tableContainer);
+    if (!reset) {
+      return;
+    }
+
+    deleteTable(tableContainer);
+
+    createTableElements(numOfTableCells, tableContainer);
+  }
+
+  function resetTable(tableContainer) {
+    if (!tableContainer.children.length) {
+      return true;
+    }
+    return window.confirm("Are you sure you want to reset the table?");
+  }
+
+  function deleteTable(tableContainer) {
     if (tableContainer.firstChild) {
       while (tableContainer.firstChild) {
         tableContainer.removeChild(tableContainer.firstChild);
       }
     }
+    return;
+  }
 
+  function createTableElements(numOfTableCells, tableContainer) {
     for (let i = 0; i < numOfTableCells; i++) {
       const cell = document.createElement("div");
       cell.className = "table__cell";
@@ -38,14 +56,5 @@ window.addEventListener("load", (event) => {
 
       tableContainer.appendChild(cell);
     }
-    console.log({ tableLength: tableContainer.children.length });
-  }
-
-  function resetTable() {
-    const tableContainer = document.querySelector(".table");
-    if (!tableContainer.children.length) {
-      return true;
-    }
-    return window.confirm("Are you sure you want to reset the table?");
   }
 });

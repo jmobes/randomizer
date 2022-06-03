65237284d482d1ae10094dbbca532b97a6427af5
Joseph Moberly <jmobes15@gmail.com>
2022-06-02 23:47:58 -0700

    Added some validation to the table input and table defaults to 100 cells

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 window.addEventListener("load", (event) => {
   console.log("page is fully loaded");
+  createTable();
 
   const tableCreateButton = document.getElementById("createTable");
   tableCreateButton.addEventListener("click", createTable);
 
-  function createTable() {
+  function createTable(num = 100) {
+    const numOfTableCells = document.getElementById("cells").value || num;
+    if (!numOfTableCells || numOfTableCells > 300 || numOfTableCells < 1)
+      return;
+
+    const reset = resetTable();
+    if (!reset) {
+      return;
+    }
+
     const tableContainer = document.querySelector(".table");
     if (tableContainer.firstChild) {
       while (tableContainer.firstChild) {
@@ -12,14 +22,30 @@ window.addEventListener("load", (event) => {
       }
     }
 
-    const numOfTableCells = document.getElementById("cells").value;
-    if (!numOfTableCells || numOfTableCells > 300) return;
-
     for (let i = 0; i < numOfTableCells; i++) {
       const cell = document.createElement("div");
       cell.className = "table__cell";
+
+      const tableCellNumber = document.createElement("div");
+      tableCellNumber.className = "table__number";
+      tableCellNumber.textContent = i + 1;
+      cell.appendChild(tableCellNumber);
+
+      const buyerName = document.createElement("div");
+      buyerName.className = "table__buyer";
+      buyerName.textContent = "Josephdfsfdsfdfsdfsdfdfs";
+      cell.appendChild(buyerName);
+
       tableContainer.appendChild(cell);
     }
     console.log({ tableLength: tableContainer.children.length });
+  }
+
+  function resetTable() {
+    const tableContainer = document.querySelector(".table");
+    if (!tableContainer.children.length) {
+      return true;
+    }
+    return window.confirm("Are you sure you want to reset the table?");
   }
 });

d449abfd09ad00ff76f662f47da52d63b81a58a0
Joseph Moberly <jmobes15@gmail.com>
2022-06-02 22:35:57 -0700

    Added function to dynamically create table of specified length

--- main.js
+++ main.js
@@ -5,7 +5,21 @@ window.addEventListener("load", (event) => {
   tableCreateButton.addEventListener("click", createTable);
 
   function createTable() {
+    const tableContainer = document.querySelector(".table");
+    if (tableContainer.firstChild) {
+      while (tableContainer.firstChild) {
+        tableContainer.removeChild(tableContainer.firstChild);
+      }
+    }
+
     const numOfTableCells = document.getElementById("cells").value;
     if (!numOfTableCells || numOfTableCells > 300) return;
+
+    for (let i = 0; i < numOfTableCells; i++) {
+      const cell = document.createElement("div");
+      cell.className = "table__cell";
+      tableContainer.appendChild(cell);
+    }
+    console.log({ tableLength: tableContainer.children.length });
   }
 });

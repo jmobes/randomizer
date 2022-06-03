8dc2970d1e68867418ba1728269b7e438d547b96
Joseph Moberly <jmobes15@gmail.com>
2022-06-03 01:29:08 -0700

    added text to show how many spots are left. Also, created functions to keep track of cell values

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 window.addEventListener("load", (event) => {
   // creates default table of size 100
-  createTable();
+  const tableSize = createTable();
+
+  // initializes array to null of set size (defaults to 100)
+  const namesArray = new Array(tableSize).fill(null);
+
+  // grabs element that displays available spots left
+  const openSpots = document.querySelector(".openSpots");
+  openSpots.textContent = `${tableSize} spots left`;
 
   // grabs the create table button and registers click event listener
   const tableCreateButton = document.getElementById("createTable");
   tableCreateButton.addEventListener("click", createTable);
+
+  // grabs the enter name + quanity submit button and registers click event listener
+  const nameSubmission = document.getElementById("names__submit");
+  nameSubmission.addEventListener("click", addNameToTable);
+
+  addNameToTable();
 
   // creates a table to be displayed on page
   function createTable(num = 100) {
@@ -21,6 +34,8 @@ window.addEventListener("load", (event) => {
     resetTable(tableContainer);
 
     createTableElements(numOfTableCells, tableContainer);
+
+    return numOfTableCells || 100;
   }
 
   // prompts user to reset table and removes it
@@ -51,8 +66,9 @@ window.addEventListener("load", (event) => {
       cell.className = "table__cell";
 
       const tableCellNumber = document.createElement("div");
-      tableCellNumber.className = "table__number";
+      tableCellNumber.className = `table__number`;
       tableCellNumber.textContent = i + 1;
+      tableCellNumber.setAttribute("data-number", i);
       cell.appendChild(tableCellNumber);
 
       const buyerName = document.createElement("div");
@@ -61,5 +77,33 @@ window.addEventListener("load", (event) => {
 
       tableContainer.appendChild(cell);
     }
+  }
+
+  function addNameToTable() {
+    const name = document.querySelector("#name").value;
+    const quantity = document.querySelector("#quantity").value || 1;
+    if (!name) return;
+    const tableCells = document.querySelectorAll(".table__number");
+    console.log({ tableCells });
+    const positions = assignRandomPosition(quantity);
+    for (let i = 0; i < positions.length; i++) {
+      const realPosition = tableCells[positions[i]].getAttribute("data-number");
+      console.log({ realPosition });
+      tableCells[realPosition].textContent = name;
+    }
+  }
+
+  function assignRandomPosition(quantity) {
+    console.log({ quantity });
+    let positionArr = [];
+    for (let i = 0; i < quantity; i++) {
+      let position = Math.floor(Math.random() * namesArray.length);
+      console.log({ position });
+      positionArr.push(position);
+      namesArray.splice(position, 1);
+      openSpots.textContent = `${namesArray.length} spots left`;
+    }
+    console.log({ positionArr });
+    return positionArr;
   }
 });

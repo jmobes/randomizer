5521d4c708c37a44296ee67fa7594b25c4915e26
Joe Moberly <jmobes15@gmail.com>
2022-06-09 23:01:24 -0700

    Added functions to table class

--- Table.js
+++ Table.js
@@ -4,6 +4,7 @@ export class Table {
         this.size = 100;
         this.names = [...Array(DEFAULT_SIZE).keys()];
         this.tableElement = document.querySelector(".table");
+        this.spotsAvailableElement = document.querySelector(".openSpots");
     }
 
     registerEventListeners = () => {
@@ -14,10 +15,10 @@ export class Table {
         if (!tableSize || tableSize > 300 || tableSize < 1) {
             return;
         }
-        this.updateNamesArray(tableSize)
+        this.start(tableSize);
     }
 
-    updateNamesArray = (tableSize) => {
+    syncNamesWithTableSize = (tableSize) => {
         if (this.names.length > tableSize) {
             const startIndex = tableSize;
             const deleteCount = this.names.length - tableSize;
@@ -73,5 +74,78 @@ export class Table {
 
           this.tableElement.appendChild(cell);
         }
+    }
+
+    updateAvailableSpots = (tableSize) => {
+        this.spotsAvailableElement.textContent = tableSize;
+    }
+
+    start = (tableSize) => {
+        this.syncNamesWithTableSize(tableSize);
+        this.resetTable();
+        this.createTableElements(tableSize);
+        this.updateAvailableSpots(tableSize);
+    }
+
+    addName = (name, quantity) => {
+        if (!name || isNaN(quantity) || quantity < 1) {
+            return;
+        }
+
+        const tableCells = this.getAllTableCells();
+        const spotsRemaining = this.names.length;
+
+        // make sure user can't enter more spots that available in the table
+        if (quantity > spotsRemaining) {
+          return;
+        }
+
+        const positions = assignRandomPosition(quantity);
+        const color = getRandomColor();
+        historyArray.push({ name, positions, color });
+        updateHistory(color.hslValues);
+        let parent;
+        let parentId;
+        for (let i = 0; i < positions.length; i++) {
+          tableCells[positions[i]].textContent = name;
+          parent = tableCells[positions[i]].parentNode;
+          parent.style.backgroundColor = color.hslString;
+          parentId = name.replaceAll(" ", "") + Date.now();
+          parent.classList.add(parentId);
+        }
+        if (parent && parentId) {
+          changeNodeColor(parentId);
+        }
+    }
+
+    getAllTableCells = () => {
+        const tableCells = document.querySelectorAll(".table__buyer");
+        return tableCells;
+    }
+
+    assignRandomPosition = (quantity) => {
+        let positionArr = [];
+        for (let i = 0; i < quantity; i++) {
+          if (!this.names.length) break;
+          let position = Math.floor(Math.random() * this.names.length);
+          positionArr.push(this.names[position]);
+          namesArray.splice(position, 1);
+          updateSpotsLeft(this.namess.length);
+        }
+        return positionArr;
+    }
+
+    getRandomColor = () => {
+        const saturation = getRandomPercentageRange(0, 100);
+        const lightness = getRandomPercentageRange(50, 100);
+        const rgb = getRandomPercentageRange(0, 360);
+        return {
+          hslString: `hsl(${rgb},${saturation}%,${lightness}%)`,
+          hslValues: { h: rgb, s: saturation, l: lightness },
+        };
+    }
+
+    getRandomPercentageRange = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
